4370c5f557cd4d03478b032fcb073eda74ca2545
Konstantin Chukharev <lipen00@gmail.com>
2023-06-13 19:46:57 +0400

    Simple example

--- examples/simple.rs
+++ examples/simple.rs
@@ -3,10 +3,6 @@ use bdd_rs::bdd::Bdd;
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
-    // simplelog::SimpleLogger::init(
-    //     simplelog::LevelFilter::Debug,
-    //     simplelog::Config::default()
-    // )?;
     simplelog::TermLogger::init(
         simplelog::LevelFilter::Debug,
         simplelog::Config::default(),
@@ -27,48 +23,12 @@ fn main() -> color_eyre::Result<()> {
     let x3 = bdd.mk_var(3);
     println!("x3 = {}", x3);
 
-    // let f = bdd.apply_ite(x1, x2, x3);
-    // println!("ite(x1, x2, x3) = {:?}", f);
-    //
-    // let g = bdd.mk_node(bdd.variable(x1.index()), x3, x2);
-    // println!("g = {:?}", g);
-
-    // let f = bdd.apply_ite(-x1, -x2, -x3);
-    // println!("f = {:?}", f);
-    // let g = bdd.mk_node(bdd.variable(x1.index()), -x2, -x3);
-    // println!("g = {:?}", g);
-
-    // let f = bdd.mk_node(bdd.variable(x1.index()), -x2, -x3);
-    // println!("f = {}", f);
-    // let g = bdd.apply_ite(-x1, -x2, -x3);
-    // println!("g = {}", g);
-    // let h = -bdd.apply_ite(x1, x3, x2);
-    // println!("h = {}", h);
-    // println!("f == g: {}", f == g);
-
-    let s = bdd.mk_node(3, -bdd.one, bdd.one);
-    let p = bdd.mk_node(2, -s, s);
-    let r = bdd.mk_node(2, s, bdd.one);
-    let q = bdd.mk_node(2, -s, bdd.one);
-    let t = bdd.mk_node(2, -bdd.one, s);
-
-    println!("s = {}", bdd.to_bracket_string(s));
-    println!("p = {}", bdd.to_bracket_string(p));
-    println!("r = {}", bdd.to_bracket_string(r));
-    println!("q = {}", bdd.to_bracket_string(q));
-    println!("t = {}", bdd.to_bracket_string(t));
-
-    println!("---");
-
-    let f = bdd.mk_node(1, -p, s);
+    let cube = bdd.cube(&[1, 2, 3]);
+    println!("cube = {}", bdd.to_bracket_string(cube));
+    let f = bdd.cube(&[2]);
     println!("f = {}", bdd.to_bracket_string(f));
-    let g = bdd.mk_node(1, -r, q);
-    println!("g = {}", bdd.to_bracket_string(g));
-    let h = bdd.mk_node(1, -bdd.one, t);
+    let h = bdd.constrain(f, cube);
     println!("h = {}", bdd.to_bracket_string(h));
-
-    let fg = bdd.constrain(f, g);
-    println!("f|g = {}", bdd.to_bracket_string(fg));
 
     Ok(())
 }
