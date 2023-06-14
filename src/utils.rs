0ef6c5131160ff01cd57d88f141cd57a541d387a
Konstantin Chukharev <lipen00@gmail.com>
2023-06-15 02:02:42 +0400

    Add Szudzik pairing function

--- src/utils.rs
+++ src/utils.rs
@@ -22,6 +22,19 @@ pub fn pairing_cantor(a: u64, b: u64) -> u64 {
     (a + b) * (a + b + 1) / 2 + b
 }
 
+/// [Szudzik pairing function][szudzik-pairing].
+///
+/// ```text
+/// (a, b) -> if (a<b) then (b^2 + a) else (a^2 + a + b)
+/// ```
+pub fn pairing_szudzik(a: u64, b: u64) -> u64 {
+    if a < b {
+        b * b + a
+    } else {
+        a * a + a + b
+    }
+}
+
 /// [Pairing function][pairing] for two `u64` values.
 ///
 /// Currently, we use Hopcroft & Ullman variant.
@@ -53,22 +66,22 @@ mod tests {
 
     #[test]
     fn test_cantor() {
-        // a\b  0  1  2  3  4  5
-        // ----------------------
-        // 0    0  2  5  9 14 20
-        // 1    1  4  8 13 19
-        // 2    3  7 12 18
-        // 3    6 11 17
-        // 4   10 16
-        // 5   15
+        // a\b  0  1  2  3  4
+        // ------------------
+        // 0    0  2  5  9 14
+        // 1    1  4  8 13
+        // 2    3  7 12
+        // 3    6 11
+        // 4   10          40
         assert_eq!(pairing_cantor(0, 0), 0);
         assert_eq!(pairing_cantor(1, 0), 1);
         assert_eq!(pairing_cantor(0, 1), 2);
         assert_eq!(pairing_cantor(2, 0), 3);
         assert_eq!(pairing_cantor(1, 1), 4);
         assert_eq!(pairing_cantor(0, 2), 5);
-        assert_eq!(pairing_cantor(5, 0), 15);
-        assert_eq!(pairing_cantor(0, 5), 20);
+        assert_eq!(pairing_cantor(4, 0), 10);
+        assert_eq!(pairing_cantor(0, 4), 14);
+        assert_eq!(pairing_cantor(4, 4), 40);
     }
 
     #[test]
@@ -79,7 +92,7 @@ mod tests {
         // 2    3  5  8 12
         // 3    6  9 13
         // 4   10 14
-        // 5   15
+        // 5   15          41
         assert_eq!(pairing_hopcroft(1, 1), 1);
         assert_eq!(pairing_hopcroft(1, 2), 2);
         assert_eq!(pairing_hopcroft(2, 1), 3);
@@ -88,5 +101,29 @@ mod tests {
         assert_eq!(pairing_hopcroft(3, 1), 6);
         assert_eq!(pairing_hopcroft(1, 5), 11);
         assert_eq!(pairing_hopcroft(5, 1), 15);
+        assert_eq!(pairing_hopcroft(5, 5), 41);
+    }
+
+    #[test]
+    fn test_szudzik() {
+        // a\b  0  1  2  3  4
+        // ------------------
+        // 0    0  1  4  9 16
+        // 1    2  3  5 10 17
+        // 2    6  7  8 11 18
+        // 3   12 13 14 15 19
+        // 4   20 21 22 23 24
+        assert_eq!(pairing_szudzik(0, 0), 0);
+        assert_eq!(pairing_szudzik(0, 1), 1);
+        assert_eq!(pairing_szudzik(1, 0), 2);
+        assert_eq!(pairing_szudzik(1, 1), 3);
+        assert_eq!(pairing_szudzik(0, 2), 4);
+        assert_eq!(pairing_szudzik(1, 2), 5);
+        assert_eq!(pairing_szudzik(2, 0), 6);
+        assert_eq!(pairing_szudzik(2, 1), 7);
+        assert_eq!(pairing_szudzik(2, 2), 8);
+        assert_eq!(pairing_szudzik(0, 4), 16);
+        assert_eq!(pairing_szudzik(4, 0), 20);
+        assert_eq!(pairing_szudzik(4, 4), 24);
     }
 }
