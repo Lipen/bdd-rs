23b822699838a03af2aaf2a8aaa34dc8f5c46b02
Konstantin Chukharev <lipen00@gmail.com>
2024-06-20 10:47:14 +0300

    Refine Expr for BDD evaluation to support complex expressions

--- src/eval.rs
+++ src/eval.rs
@@ -15,64 +15,151 @@ impl Bdd {
 
 impl Eval for Ref {
     fn eval(&self, _bdd: &Bdd) -> Ref {
-        self.clone()
+        *self
     }
 }
 
-pub struct BddAndOp {
-    f: Ref,
-    g: Ref,
+pub enum Expr {
+    Term(Ref),
+    And(Box<Expr>, Box<Expr>),
+    Or(Box<Expr>, Box<Expr>),
+    Xor(Box<Expr>, Box<Expr>),
 }
 
-impl Mul for Ref {
-    type Output = BddAndOp;
+impl Expr {
+    pub fn term(value: Ref) -> Self {
+        Expr::Term(value)
+    }
+
+    pub fn and(lhs: Self, rhs: Self) -> Self {
+        Expr::And(Box::new(lhs), Box::new(rhs))
+    }
+
+    pub fn or(lhs: Self, rhs: Self) -> Self {
+        Expr::Or(Box::new(lhs), Box::new(rhs))
+    }
+
+    pub fn xor(lhs: Self, rhs: Self) -> Self {
+        Expr::Xor(Box::new(lhs), Box::new(rhs))
+    }
+}
+
+impl Eval for Expr {
+    fn eval(&self, bdd: &Bdd) -> Ref {
+        match self {
+            Expr::Term(term) => *term,
+            Expr::And(lhs, rhs) => bdd.apply_and(lhs.eval(bdd), rhs.eval(bdd)),
+            Expr::Or(lhs, rhs) => bdd.apply_or(lhs.eval(bdd), rhs.eval(bdd)),
+            Expr::Xor(lhs, rhs) => bdd.apply_xor(lhs.eval(bdd), rhs.eval(bdd)),
+        }
+    }
+}
+
+// Expr * Expr = Expr::And
+impl Mul for Expr {
+    type Output = Expr;
 
     fn mul(self, rhs: Self) -> Self::Output {
-        BddAndOp { f: self, g: rhs }
+        Expr::and(self, rhs)
     }
 }
 
-impl Eval for BddAndOp {
-    fn eval(&self, bdd: &Bdd) -> Ref {
-        bdd.apply_and(self.f, self.g)
+// Ref * Expr = Expr::And
+impl Mul<Expr> for Ref {
+    type Output = Expr;
+
+    fn mul(self, rhs: Expr) -> Self::Output {
+        Expr::and(Expr::term(self), rhs)
     }
 }
 
-pub struct BddOrOp {
-    f: Ref,
-    g: Ref,
+// Expr * Ref = Expr::And
+impl Mul<Ref> for Expr {
+    type Output = Expr;
+
+    fn mul(self, rhs: Ref) -> Self::Output {
+        Expr::and(self, Expr::term(rhs))
+    }
 }
 
-impl Add for Ref {
-    type Output = BddOrOp;
+// Ref * Ref = Expr::And
+impl Mul for Ref {
+    type Output = Expr;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Expr::and(Expr::term(self), Expr::term(rhs))
+    }
+}
+
+// Expr + Expr = Expr::Or
+impl Add for Expr {
+    type Output = Expr;
 
     fn add(self, rhs: Self) -> Self::Output {
-        BddOrOp { f: self, g: rhs }
+        Expr::or(self, rhs)
     }
 }
 
-impl Eval for BddOrOp {
-    fn eval(&self, bdd: &Bdd) -> Ref {
-        bdd.apply_or(self.f, self.g)
+// Ref + Expr = Expr::Or
+impl Add<Expr> for Ref {
+    type Output = Expr;
+
+    fn add(self, rhs: Expr) -> Self::Output {
+        Expr::or(Expr::term(self), rhs)
     }
 }
 
-pub struct BddXorOp {
-    f: Ref,
-    g: Ref,
+// Expr + Ref = Expr::Or
+impl Add<Ref> for Expr {
+    type Output = Expr;
+
+    fn add(self, rhs: Ref) -> Self::Output {
+        Expr::or(self, Expr::term(rhs))
+    }
 }
 
-impl BitXor for Ref {
-    type Output = BddXorOp;
+// Ref + Ref = Expr::Or
+impl Add for Ref {
+    type Output = Expr;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Expr::or(Expr::term(self), Expr::term(rhs))
+    }
+}
+
+// Expr ^ Expr = Expr::Xor
+impl BitXor for Expr {
+    type Output = Expr;
 
     fn bitxor(self, rhs: Self) -> Self::Output {
-        BddXorOp { f: self, g: rhs }
+        Expr::xor(self, rhs)
     }
 }
 
-impl Eval for BddXorOp {
-    fn eval(&self, bdd: &Bdd) -> Ref {
-        bdd.apply_xor(self.f, self.g)
+// Ref ^ Expr = Expr::Xor
+impl BitXor<Expr> for Ref {
+    type Output = Expr;
+
+    fn bitxor(self, rhs: Expr) -> Self::Output {
+        Expr::xor(Expr::term(self), rhs)
+    }
+}
+
+// Expr ^ Ref = Expr::Xor
+impl BitXor<Ref> for Expr {
+    type Output = Expr;
+
+    fn bitxor(self, rhs: Ref) -> Self::Output {
+        Expr::xor(self, Expr::term(rhs))
+    }
+}
+
+// Ref ^ Ref = Expr::Xor
+impl BitXor for Ref {
+    type Output = Expr;
+
+    fn bitxor(self, rhs: Self) -> Self::Output {
+        Expr::xor(Expr::term(self), Expr::term(rhs))
     }
 }
 
@@ -125,6 +212,18 @@ mod tests {
         let y = bdd.mk_var(2);
         let f = bdd.apply_xor(x, y);
         let res = bdd.eval(x ^ y);
+        assert_eq!(res, f);
+    }
+
+    #[test]
+    fn test_eval_complex() {
+        let bdd = Bdd::default();
+        let x = bdd.mk_var(1);
+        let y = bdd.mk_var(2);
+        let z = bdd.mk_var(3);
+        let t = bdd.mk_var(4);
+        let f = bdd.apply_xor(bdd.apply_or(x, y), bdd.apply_and(z, t));
+        let res = bdd.eval(x + y ^ z * t);
         assert_eq!(res, f);
     }
 }
