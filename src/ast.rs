b8437c380f295d7ab5449011e34846f72d74f087
Konstantin Chukharev <lipen00@gmail.com>
2024-06-22 19:16:10 +0300

    Add Functor, refine Arena, use refs instead of Clone

--- src/ast.rs
+++ src/ast.rs
@@ -1,8 +1,6 @@
 use std::collections::VecDeque;
 use std::ops::{Add, BitAnd, BitOr, BitXor, Mul, Neg};
 
-use num_traits::Zero;
-
 #[derive(Debug, Clone)]
 pub enum ExprBoxed<T> {
     Term(T),
@@ -47,35 +45,47 @@ impl<T> ExprBoxed<T> {
 pub struct Idx(usize);
 
 #[derive(Debug)]
-pub enum Expr<T, I = Idx> {
+pub enum Expr<T, N = Idx> {
     Term(T),
-    Not(I),
-    And(I, I),
-    Or(I, I),
-    Xor(I, I),
-    Ite(I, I, I),
+    Not(N),
+    And(N, N),
+    Or(N, N),
+    Xor(N, N),
+    Ite(N, N, N),
 }
 
-impl<T, A> Expr<T, A> {
-    #[inline(always)]
-    pub fn fmap<B, F>(self, mut f: F) -> Expr<T, B>
-    where
-        F: FnMut(A) -> B,
-    {
-        match self {
-            Expr::Term(a) => Expr::Term(a),
-            Expr::Not(a) => Expr::Not(f(a)),
-            Expr::And(a, b) => Expr::And(f(a), f(b)),
-            Expr::Or(a, b) => Expr::Or(f(a), f(b)),
-            Expr::Xor(a, b) => Expr::Xor(f(a), f(b)),
-            Expr::Ite(a, b, c) => Expr::Ite(f(a), f(b), f(c)),
-        }
-    }
+pub trait Functor {
+    type Inner;
+    type Mapped<R>: Functor;
 
-    #[inline(always)]
-    pub fn fmap_ref<B, F>(&self, mut f: F) -> Expr<&T, B>
+    fn fmap<R, F>(self, f: F) -> Self::Mapped<R>
     where
-        F: FnMut(&A) -> B,
+        F: FnMut(Self::Inner) -> R;
+}
+
+// Just an example of how to implement the Functor trait for a type.
+impl<T> Functor for Option<T> {
+    type Inner = T;
+    type Mapped<R> = Option<R>;
+
+    fn fmap<R, F>(self, mut f: F) -> Self::Mapped<R>
+    where
+        F: FnMut(Self::Inner) -> R,
+    {
+        match self {
+            Some(inner) => Some(f(inner)),
+            None => None,
+        }
+    }
+}
+
+impl<T, N> Functor for Expr<T, N> {
+    type Inner = N;
+    type Mapped<R> = Expr<T, R>;
+
+    fn fmap<R, F>(self, mut f: F) -> Self::Mapped<R>
+    where
+        F: FnMut(Self::Inner) -> R,
     {
         match self {
             Expr::Term(a) => Expr::Term(a),
@@ -86,202 +96,53 @@ impl<T, A> Expr<T, A> {
             Expr::Ite(a, b, c) => Expr::Ite(f(a), f(b), f(c)),
         }
     }
+}
+
+impl<'a, T, N> Functor for &'a Expr<T, N> {
+    type Inner = &'a N;
+    type Mapped<R> = Expr<&'a T, R>;
+
+    fn fmap<R, F>(self, mut f: F) -> Self::Mapped<R>
+    where
+        F: FnMut(Self::Inner) -> R,
+    {
+        match self {
+            Expr::Term(a) => Expr::Term(a),
+            Expr::Not(a) => Expr::Not(f(a)),
+            Expr::And(a, b) => Expr::And(f(a), f(b)),
+            Expr::Or(a, b) => Expr::Or(f(a), f(b)),
+            Expr::Xor(a, b) => Expr::Xor(f(a), f(b)),
+            Expr::Ite(a, b, c) => Expr::Ite(f(a), f(b), f(c)),
+        }
+    }
+}
+
+pub trait Tree {
+    type Leaf;
+    type Node;
+}
+
+impl<T, N> Tree for Expr<T, N> {
+    type Leaf = T;
+    type Node = N;
 }
 
 // See: https://recursion.wtf/posts/rust_schemes/
 #[derive(Debug)]
-pub struct ExprArena<T> {
-    /// Topology sorted expressions, by construction.
-    exprs: Vec<Expr<T>>,
-}
-
-impl<T> ExprArena<T> {
-    pub fn get(&self, idx: Idx) -> &Expr<T> {
-        &self.exprs[idx.0]
-    }
-
-    pub fn get_mut(&mut self, idx: Idx) -> &mut Expr<T> {
-        &mut self.exprs[idx.0]
-    }
-}
-
-impl<T> ExprArena<T> {
-    fn expand_exprs<R, F>(seed: R, expand: F) -> Self
-    where
-        R: Clone,
-        F: Fn(R) -> Expr<T, R>,
-    {
-        let mut frontier: VecDeque<R> = VecDeque::from([seed]);
-        let mut exprs: Vec<Expr<T>> = vec![];
-
-        while let Some(seed) = frontier.pop_front() {
-            let expr = expand(seed);
-            let expr = expr.fmap(|e| {
-                frontier.push_back(e.clone());
-                Idx(exprs.len() + frontier.len())
-            });
-            exprs.push(expr);
-        }
-
-        Self { exprs }
-    }
-
-    pub fn from_boxed(ast: &ExprBoxed<T>) -> Self
-    where
-        T: Clone,
-    {
-        Self::expand_exprs(ast, |seed| match seed {
-            ExprBoxed::Term(term) => Expr::Term(term.clone()),
-            ExprBoxed::Not(a) => Expr::Not(a),
-            ExprBoxed::And(a, b) => Expr::And(a, b),
-            ExprBoxed::Or(a, b) => Expr::Or(a, b),
-            ExprBoxed::Xor(a, b) => Expr::Xor(a, b),
-            ExprBoxed::Ite(a, b, c) => Expr::Ite(a, b, c),
-        })
-    }
-}
-
-impl<T> ExprArena<T> {
-    fn collapse_exprs<R, F>(&self, mut collapse: F) -> R
-    where
-        F: FnMut(Expr<&T, R>) -> R,
-    {
-        let mut results: Vec<Option<R>> = std::iter::repeat_with(|| None)
-            .take(self.exprs.len())
-            .collect();
-
-        for (i, expr) in self.exprs.iter().enumerate().rev() {
-            let expr = expr.fmap_ref(|idx| results[idx.0].take().unwrap());
-            let result = collapse(expr);
-            results[i] = Some(result);
-        }
-
-        results.into_iter().next().unwrap().unwrap()
-    }
-
-    pub fn eval(&self) -> T
-    where
-        T: Clone,
-        T: Neg<Output = T>,
-        // T: BitAnd<Output=T>,
-        // T: BitOr<Output=T>,
-        T: Mul<Output = T>, // temporarily
-        T: Add<Output = T>, // temporarily
-        T: BitXor<Output = T>,
-        T: Zero,
-    {
-        self.collapse_exprs::<T, _>(|expr| match expr {
-            Expr::Term(term) => term.clone(),
-            Expr::Not(a) => -a,
-            Expr::And(a, b) => a * b,
-            Expr::Or(a, b) => a + b,
-            Expr::Xor(a, b) => a ^ b,
-            Expr::Ite(a, b, c) => {
-                if a.is_zero() {
-                    b
-                } else {
-                    c
-                }
-            }
-        })
-    }
-
-    pub fn to_boxed(&self) -> ExprBoxed<T>
-    where
-        T: Clone,
-    {
-        self.collapse_exprs::<ExprBoxed<T>, _>(|expr| match expr {
-            Expr::Term(term) => ExprBoxed::term(term.clone()),
-            Expr::Not(a) => ExprBoxed::not(a),
-            Expr::And(a, b) => ExprBoxed::and(a, b),
-            Expr::Or(a, b) => ExprBoxed::or(a, b),
-            Expr::Xor(a, b) => ExprBoxed::xor(a, b),
-            Expr::Ite(a, b, c) => ExprBoxed::ite(a, b, c),
-        })
-    }
-}
-
-pub trait FMap<A> {
-    type Output<B>;
-
-    fn fmap<B, F>(self, f: F) -> Self::Output<B>
-    where
-        F: FnMut(A) -> B;
-}
-
-pub trait FMapRef<A> {
-    type Output<'a, B>
-    where
-        Self: 'a;
-
-    fn fmap_ref<B, F>(&self, f: F) -> Self::Output<'_, B>
-    where
-        F: FnMut(&A) -> B;
-}
-
-pub trait GenericExpr<A>: FMap<A> + FMapRef<A> {
-    type Term;
-}
-
-#[derive(Debug)]
-pub enum MyExpr<T, A = Idx> {
-    Term(T),
-    And(A, A),
-    Or(A, A),
-}
-
-impl<T, A> FMap<A> for MyExpr<T, A> {
-    type Output<B> = MyExpr<T, B>;
-
-    fn fmap<B, F>(self, mut f: F) -> Self::Output<B>
-    where
-        F: FnMut(A) -> B,
-    {
-        match self {
-            MyExpr::Term(t) => MyExpr::Term(t),
-            MyExpr::And(a, b) => MyExpr::And(f(a), f(b)),
-            MyExpr::Or(a, b) => MyExpr::Or(f(a), f(b)),
-        }
-    }
-}
-
-impl<T, A> FMapRef<A> for MyExpr<T, A> {
-    type Output<'a, B> = MyExpr<&'a T, B>
-    where
-        T: 'a,
-        A: 'a;
-
-    fn fmap_ref<B, F>(&self, mut f: F) -> Self::Output<'_, B>
-    where
-        F: FnMut(&A) -> B,
-    {
-        match self {
-            MyExpr::Term(t) => MyExpr::<&T, B>::Term(t),
-            MyExpr::And(a, b) => MyExpr::And(f(a), f(b)),
-            MyExpr::Or(a, b) => MyExpr::Or(f(a), f(b)),
-        }
-    }
-}
-
-impl<T, A> GenericExpr<A> for MyExpr<T, A> {
-    type Term = T;
-}
-
-#[derive(Debug)]
-pub struct GenericExprArena<E> {
+pub struct Arena<E> {
     exprs: Vec<E>,
 }
 
-impl<E> GenericExprArena<E>
+impl<E> Arena<E>
 where
-    E: GenericExpr<Idx>,
+    E: Tree,
 {
-    fn expand_exprs<R, X, F>(seed: R, expand: F) -> Self
+    fn expand_exprs<S, R, F>(seed: S, expand: F) -> Self
     where
-        X: FMap<R, Output<Idx> = E>,
-        F: Fn(R) -> X,
+        R: Functor<Inner = S, Mapped<Idx> = E>,
+        F: Fn(S) -> R,
     {
-        let mut frontier: VecDeque<R> = VecDeque::from([seed]);
+        let mut frontier: VecDeque<S> = VecDeque::from([seed]);
         let mut exprs: Vec<E> = vec![];
 
         while let Some(seed) = frontier.pop_front() {
@@ -297,64 +158,36 @@ where
     }
 }
 
-impl<T> GenericExprArena<MyExpr<T>> {
-    pub fn from_boxed(ast: ExprBoxed<T>) -> Self {
-        Self::expand_exprs(ast, |seed| match seed {
-            ExprBoxed::Term(term) => MyExpr::Term(term),
-            // ExprBoxed::Not(a) => MyExpr::Not(a),
-            ExprBoxed::And(a, b) => MyExpr::And(*a, *b),
-            ExprBoxed::Or(a, b) => MyExpr::Or(*a, *b),
-            // ExprBoxed::Xor(a, b) => MyExpr::Xor(a, b),
-            // ExprBoxed::Ite(a, b, c) => MyExpr::Ite(a, b, c),
-            _ => todo!(),
-        })
-    }
-
-    pub fn from_boxed_ref(ast: &ExprBoxed<T>) -> Self
+impl<T> Arena<Expr<T>> {
+    pub fn from_boxed(ast: &ExprBoxed<T>) -> Self
     where
         T: Clone,
     {
         Self::expand_exprs(ast, |seed| match seed {
-            ExprBoxed::Term(term) => MyExpr::Term(term.clone()),
-            // ExprBoxed::Not(a) => MyExpr::Not(a),
-            ExprBoxed::And(a, b) => MyExpr::And(&**a, &**b),
-            ExprBoxed::Or(a, b) => MyExpr::Or(&**a, &**b),
-            // ExprBoxed::Xor(a, b) => MyExpr::Xor(a, b),
-            // ExprBoxed::Ite(a, b, c) => MyExpr::Ite(a, b, c),
-            _ => todo!(),
+            ExprBoxed::Term(term) => Expr::Term(term.clone()),
+            ExprBoxed::Not(a) => Expr::Not(&**a),
+            ExprBoxed::And(a, b) => Expr::And(&**a, &**b),
+            ExprBoxed::Or(a, b) => Expr::Or(&**a, &**b),
+            ExprBoxed::Xor(a, b) => Expr::Xor(&**a, &**b),
+            ExprBoxed::Ite(a, b, c) => Expr::Ite(&**a, &**b, &**c),
         })
     }
 }
 
-impl<T> From<ExprBoxed<T>> for GenericExprArena<MyExpr<T>> {
-    fn from(value: ExprBoxed<T>) -> Self {
-        Self::from_boxed(value)
-    }
-}
-
-impl<T> From<&ExprBoxed<T>> for GenericExprArena<MyExpr<T>>
+impl<'a, E: 'a> Arena<E>
 where
-    T: Clone,
+    &'a E: Functor<Inner = &'a Idx>,
 {
-    fn from(value: &ExprBoxed<T>) -> Self {
-        Self::from_boxed_ref(value)
-    }
-}
-
-impl<E> GenericExprArena<E>
-where
-    E: GenericExpr<Idx>,
-{
-    fn collapse_exprs<'a, B, F>(&'a self, mut collapse: F) -> B
+    fn collapse_exprs<R, F>(&'a self, mut collapse: F) -> R
     where
-        F: FnMut(<E as FMapRef<Idx>>::Output<'a, B>) -> B,
+        F: FnMut(<&'a E as Functor>::Mapped<R>) -> R,
     {
-        let mut results: Vec<Option<B>> = std::iter::repeat_with(|| None)
+        let mut results: Vec<Option<R>> = std::iter::repeat_with(|| None)
             .take(self.exprs.len())
             .collect();
 
         for (i, expr) in self.exprs.iter().enumerate().rev() {
-            let expr = expr.fmap_ref(|idx| results[idx.0].take().unwrap());
+            let expr = expr.fmap(|idx| results[idx.0].take().unwrap());
             let result = collapse(expr);
             results[i] = Some(result);
         }
@@ -363,7 +196,23 @@ where
     }
 }
 
-impl<T> GenericExprArena<MyExpr<T>> {
+impl<T> Arena<Expr<T>> {
+    pub fn to_boxed(&self) -> ExprBoxed<T>
+    where
+        T: Clone,
+    {
+        self.collapse_exprs(|expr| match expr {
+            Expr::Term(term) => ExprBoxed::term(term.clone()),
+            Expr::Not(a) => ExprBoxed::not(a),
+            Expr::And(a, b) => ExprBoxed::and(a, b),
+            Expr::Or(a, b) => ExprBoxed::or(a, b),
+            Expr::Xor(a, b) => ExprBoxed::xor(a, b),
+            Expr::Ite(a, b, c) => ExprBoxed::ite(a, b, c),
+        })
+    }
+}
+
+impl<T> Arena<Expr<T>> {
     pub fn eval(&self) -> T
     where
         T: Clone,
@@ -371,22 +220,10 @@ impl<T> GenericExprArena<MyExpr<T>> {
         T: Add<Output = T>,
     {
         self.collapse_exprs(|expr| match expr {
-            MyExpr::Term(term) => term.clone(),
-            MyExpr::And(a, b) => a * b,
-            MyExpr::Or(a, b) => a + b,
-        })
-    }
-}
-
-impl<T> GenericExprArena<MyExpr<T>> {
-    pub fn to_boxed(&self) -> ExprBoxed<T>
-    where
-        T: Clone,
-    {
-        self.collapse_exprs(|expr| match expr {
-            MyExpr::Term(term) => ExprBoxed::term(term.clone()),
-            MyExpr::And(a, b) => ExprBoxed::and(a, b),
-            MyExpr::Or(a, b) => ExprBoxed::or(a, b),
+            Expr::Term(term) => term.clone(),
+            Expr::And(a, b) => a * b,
+            Expr::Or(a, b) => a + b,
+            _ => todo!(),
         })
     }
 }
@@ -402,32 +239,28 @@ mod tests {
             ExprBoxed::or(ExprBoxed::term(2), ExprBoxed::Term(3)),
         );
         println!("expr_boxed = {:?}", expr_boxed);
-        let arena = ExprArena::from_boxed(&expr_boxed);
+        let arena = Arena::from_boxed(&expr_boxed);
         println!("arena = {:?}", arena);
         for expr in arena.exprs.iter() {
             println!("- {:?}", expr);
         }
-        println!("eval = {}", arena.eval());
-        println!("eval = {}", arena.eval());
         println!("eval = {}", arena.eval());
         assert_eq!(arena.eval(), 4 * (2 + 3));
         println!("boxed = {:?}", arena.to_boxed());
     }
 
     #[test]
-    fn test_generic_expr() {
+    fn test_generic() {
         let expr_boxed = ExprBoxed::and(
             ExprBoxed::term(4),
             ExprBoxed::or(ExprBoxed::term(2), ExprBoxed::Term(3)),
         );
         println!("expr_boxed = {:?}", expr_boxed);
-        let arena = GenericExprArena::from_boxed_ref(&expr_boxed);
+        let arena = Arena::from_boxed(&expr_boxed);
         println!("arena = {:?}", arena);
         for expr in arena.exprs.iter() {
             println!("- {:?}", expr);
         }
-        println!("eval = {}", arena.eval());
-        println!("eval = {}", arena.eval());
         println!("eval = {}", arena.eval());
         assert_eq!(arena.eval(), 4 * (2 + 3));
         println!("boxed = {:?}", arena.to_boxed());
