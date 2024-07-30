a9e030cd73e66daad9f91d898021fab4c0a01aa9
Konstantin Chukharev <lipen00@gmail.com>
2024-07-30 23:57:31 +0300

    Change inner type for Ref to u32

--- src/reference.rs
+++ src/reference.rs
@@ -3,35 +3,30 @@ use std::ops::Neg;
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 #[repr(transparent)]
-pub struct Ref(i32);
+pub struct Ref(u32);
 
-//noinspection RsAssertEqual (see https://github.com/rust-lang/rust/issues/119826)
 impl Ref {
     pub const ZERO: Self = Self(0);
 
-    pub const fn new(value: i32) -> Self {
-        assert!(value != 0, "Reference cannot be zero, use Ref::ZERO instead");
-        Self(value)
+    pub const fn new(index: u32, negated: bool) -> Self {
+        debug_assert!(index < 0x8000_0000, "Index is too large");
+        Self((index << 1) | (negated as u32))
     }
-    pub const fn positive(value: u32) -> Self {
-        assert!(value != 0, "Reference cannot be zero, use Ref::ZERO instead");
-        Self(value as i32)
+    pub const fn positive(index: u32) -> Self {
+        Self::new(index, false)
     }
-    pub const fn negative(value: u32) -> Self {
-        assert!(value != 0, "Reference cannot be zero, use Ref::ZERO instead");
-        Self(-(value as i32))
+    pub const fn negative(index: u32) -> Self {
+        Self::new(index, true)
     }
-
     pub const fn index(self) -> u32 {
-        self.0.unsigned_abs()
+        self.0 >> 1
     }
     pub const fn is_negated(self) -> bool {
-        self.0 < 0
+        (self.0 & 1) != 0
     }
 
     pub(crate) const fn hashy(self) -> u64 {
-        (self.index() << 1) as u64 | self.is_negated() as u64
-        // self.index() as u64
+        self.0 as u64
     }
 }
 
@@ -40,7 +35,7 @@ impl Neg for Ref {
     type Output = Self;
 
     fn neg(self) -> Self::Output {
-        Self(-self.0)
+        Self(self.0 ^ 1)
     }
 }
 
