aae9c88b3e8a4d039a087b94dfa616d49d758c0a
Konstantin Chukharev <lipen00@gmail.com>
2025-12-02 13:52:07 +0300

    Remove Default bound

--- src/cache/direct_mapped.rs
+++ src/cache/direct_mapped.rs
@@ -144,8 +144,8 @@ impl<K: MyHash, V> DirectMappedCache<K, V> {
 
 impl<K, V> DirectMappedCache<K, V>
 where
-    K: MyHash + Eq + Default,
-    V: Copy + Default,
+    K: MyHash + Eq,
+    V: Copy,
 {
     /// Looks up a key in the cache.
     #[inline]
